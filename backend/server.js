a9623696cc50344af195f229222c920a44730324
Ujjwal Prajapati <ujjwalprajapati154@gmail.com>
2024-03-17 16:18:23 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -12,6 +13,8 @@ import { app, server } from "./socket/socket.js";
 // const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); // Middleware for parsing JSON bodies with JSON payloads (From req.body)
@@ -21,12 +24,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//     res.send("Hello World!!");
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
-    connectToMonogoDB();
-    console.log(`Server is running on http://localhost:${PORT}`);
+  connectToMonogoDB();
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
